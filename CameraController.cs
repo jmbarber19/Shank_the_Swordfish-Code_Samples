68192146a96747e037c2a422b5f11d2fdfda8766
waterbuddy16 <watercalciumandchlorides@gmail.com>
2019-03-10 15:30:38 -0400

    tabs converted to spaces

--- CameraController.cs
+++ CameraController.cs
@@ -6,38 +6,38 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-	[Header("Camera Variables")]
+    [Header("Camera Variables")]
 
     [SerializeField] [Tooltip("Regardless of velocity, how far should the camera look ahead")]
-	private float lookAheadConstant = 0.3f;
+    private float lookAheadConstant = 0.3f;
     [SerializeField] [Tooltip("How much the velocity should influence camera look ahead")]
-	private float lookAheadVelocityInfluence = 0.0125f;
+    private float lookAheadVelocityInfluence = 0.0125f;
     [SerializeField] [Tooltip("How fast does the camera interpolate to the desired position")]
-	private float lookAheadChangeSpeed = 5f;
+    private float lookAheadChangeSpeed = 5f;
 
 
-	[Header("Camera Variables")]
+    [Header("Camera Variables")]
 
     [SerializeField] [Tooltip("How much velocity affects the cameras Z position")]
-	private float playerVelocityZInfluence = 0.25f;
+    private float playerVelocityZInfluence = 0.25f;
     [SerializeField] [Tooltip("How quickly the cameras Z position will move to the desired position")]
-	private float zChangeSpeed = 0.2f;
+    private float zChangeSpeed = 0.2f;
     [SerializeField] [Tooltip("How much outside influence can change the fov from its default value")]
     private Vector2 fovAddLimits = new Vector2(0f, 5f);
 
 
     private Camera camera;
     // Currently how far ahead the camera is looking past the player trowards the mouse
-	private float currentCameraLookAhead = 0f;
-	private float defaultZPosition = 0f;
-	private float defaultFOV = 0f;
+    private float currentCameraLookAhead = 0f;
+    private float defaultZPosition = 0f;
+    private float defaultFOV = 0f;
 
 
     /*
      * Executed on start of scene containing gameobject with this CameraController Component
      * Used to intialize the camera, z position, and default field of view
      */
-	private void Start ()
+    private void Start ()
     {
         camera = GetComponent<Camera>();
         defaultZPosition = transform.localPosition.z;
@@ -49,11 +49,11 @@ public class CameraController : MonoBehaviour
      * Executed on every update of the game loop
      * Sets the cameras position and field of view
      */
-	private void Update ()
+    private void Update ()
     {
-		SetPosition ();
+        SetPosition ();
         SetFieldOfView ();
-	}
+    }
 
 
     /*
@@ -63,19 +63,19 @@ public class CameraController : MonoBehaviour
     {
         Vector3 playerVelocity = GameManager.Instance().GetPlayerVelocity();
 
-		currentCameraLookAhead = Mathf.Lerp (
+        currentCameraLookAhead = Mathf.Lerp (
             currentCameraLookAhead,
             lookAheadConstant + playerVelocity.magnitude * lookAheadVelocityInfluence,
             Time.deltaTime * lookAheadChangeSpeed
         );
 
-		Vector3 playerDirection = GetMouseTarget() - GameManager.Instance().GetPlayerPosition();
+        Vector3 playerDirection = GetMouseTarget() - GameManager.Instance().GetPlayerPosition();
         playerDirection.Normalize();
         
         // Get a position the camera should be at, but don't overwrite current values yet
-		Vector3 goalPosition = (GameManager.Instance().GetPlayerPosition() * 2f + playerDirection * currentCameraLookAhead) / 2f;
+        Vector3 goalPosition = (GameManager.Instance().GetPlayerPosition() * 2f + playerDirection * currentCameraLookAhead) / 2f;
 
-		goalPosition.z = Mathf.Lerp(
+        goalPosition.z = Mathf.Lerp(
             transform.localPosition.z,
             (playerVelocity.magnitude * playerVelocityZInfluence) * Mathf.Sign(defaultZPosition) + defaultZPosition,
             Time.deltaTime * zChangeSpeed
@@ -92,7 +92,7 @@ public class CameraController : MonoBehaviour
     {
         Vector3 playerVelocity = GameManager.Instance().GetPlayerVelocity();
         
-		camera.fieldOfView = Mathf.Lerp(
+        camera.fieldOfView = Mathf.Lerp(
             camera.fieldOfView,
             defaultFOV + Mathf.Clamp(playerVelocity.magnitude * playerVelocityZInfluence, fovAddLimits.x, fovAddLimits.y),
             Time.deltaTime
@@ -106,7 +106,7 @@ public class CameraController : MonoBehaviour
     public Vector3 GetMouseTarget ()
     {
         Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-		target.z = 0f;
+        target.z = 0f;
         return target;
     }
 
--- PlayerController.cs
+++ PlayerController.cs
@@ -7,40 +7,40 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-	[Header("Player Controls")]
+    [Header("Player Controls")]
     [SerializeField] [Tooltip("Mouse click number (0 = left, 1 = right, 2 = middle, etc)")]
     private int mouseButtonClickId = 0;
     [SerializeField] [Tooltip("Time between two clicks to count as a double click")]
-	private float doubleClickTime = 0.2f;
+    private float doubleClickTime = 0.2f;
 
-	[Header("Movement")]
+    [Header("Movement")]
     [SerializeField] [Tooltip("How quickly the player accelerates")]
-	private float accelerationIntensity = 30f;
+    private float accelerationIntensity = 30f;
     [SerializeField]
-	private float maxVelocity = 30f;
+    private float maxVelocity = 30f;
     [SerializeField]
-	private float maxDashVelocity = 50f;
+    private float maxDashVelocity = 50f;
     [SerializeField] [Tooltip("Speed at which the player will slow down to 0 velocity")]
-	private float dragSpeed = 0.5f;
+    private float dragSpeed = 0.5f;
     [SerializeField] [Tooltip("If facing opposite direction of velocity, use this a accel boost (should have x values in a range of -1 to 1 with y values greater than 1)")]
     private AnimationCurve oppsiteAccelerationBoost = null;
     [SerializeField] [Tooltip("Speed the player to rotates towards the mouse at")]
-	private float rotateSpeed = 10f;
+    private float rotateSpeed = 10f;
     [SerializeField] [Tooltip("Speed dampen after bouncing off a wall")]
-	private float bounceSpeedDampen = 0.5f;
+    private float bounceSpeedDampen = 0.5f;
     [SerializeField] [Tooltip("Time to disable the player from moving when hitting a wall")]
-	private float disableMoveMaxTime = 0.3f;
+    private float disableMoveMaxTime = 0.3f;
     [SerializeField]
-	private Vector3 outOfWaterGravity = new Vector3(0f, -30f, 0f);
+    private Vector3 outOfWaterGravity = new Vector3(0f, -30f, 0f);
     [SerializeField] [Tooltip("Water level which the player will begin using gravity after this point")]
     private float topWaterY = 15f;
 
 
     [Header("Ranking")]
     [SerializeField] [Tooltip("How the current combo rank influences the players speed/ accel")]
-	private float maxVelocityRankMultiplier = 3f;
+    private float maxVelocityRankMultiplier = 3f;
     [SerializeField] [Tooltip("How much acceleration increases based on the players current rank")]
-	private float accelerationRankMultiplier = 0f;
+    private float accelerationRankMultiplier = 0f;
 
 
     [Header("Other")]
@@ -49,57 +49,57 @@ public class PlayerController : MonoBehaviour {
 
 
 
-	private Animator animator;
+    private Animator animator;
 
-	private Vector3 direction = Vector3.zero;
-	private Vector3 velocity = Vector3.zero;
-	private Vector3 acceleration = Vector3.zero;
-	private float currentMaxVelocity = 0f;
+    private Vector3 direction = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 acceleration = Vector3.zero;
+    private float currentMaxVelocity = 0f;
 
-	private float disableMoveTimer = 0f; // Current time that the player is disabled
-	private bool outOfWater = false;
+    private float disableMoveTimer = 0f; // Current time that the player is disabled
+    private bool outOfWater = false;
 
-	private Vector2 clickTimeHistory = Vector2.zero; // Past two click times
+    private Vector2 clickTimeHistory = Vector2.zero; // Past two click times
     private bool isClicking = false;
     private bool isDoubleClicking = false;
 
 
-	/*
+    /*
      * Gets the current players current velocity
      * 
      * @return Vector3 - player velocity
      */
-	public Vector3 GetVelocity()
-	{
-		return velocity;
-	}
+    public Vector3 GetVelocity()
+    {
+        return velocity;
+    }
 
 
     /*
      * Executed on start of scene containing gameobject with this PlayerController Component
      * Used to intialize the variables
      */
-	private void Start () {
-		animator = GetComponent<Animator> ();
+    private void Start () {
+        animator = GetComponent<Animator> ();
 
-		currentMaxVelocity = maxVelocity;
-	}
-	
+        currentMaxVelocity = maxVelocity;
+    }
+    
 
     /*
      * Executed on every update of the game loop
      * Calls relevant methods to handle player control and other relevant details
      */
-	private void Update () {
+    private void Update () {
 
         // If the game is paused don't allow the player to do anything
-		if (Time.timeScale == 0f) {
-			return;
-		}
+        if (Time.timeScale == 0f) {
+            return;
+        }
 
-		outOfWater = transform.position.y > topWaterY;
+        outOfWater = transform.position.y > topWaterY;
 
-		direction = GameManager.Instance().GetCameraController().GetMouseTarget() - transform.position;
+        direction = GameManager.Instance().GetCameraController().GetMouseTarget() - transform.position;
         direction.Normalize();
 
         HandleInput();
@@ -107,12 +107,12 @@ public class PlayerController : MonoBehaviour {
         HandleReflectonOnCollision();
         HandleRotation();
 
-		SetAcceleration();
-		SetVelocity();
+        SetAcceleration();
+        SetVelocity();
 
         HandleDash();
 
-		transform.position += velocity * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
 
         FaceSpriteInDirection();
         
@@ -121,7 +121,7 @@ public class PlayerController : MonoBehaviour {
             SetAnimatorState();
         }
 
-	}
+    }
 
 
     /*
@@ -151,7 +151,7 @@ public class PlayerController : MonoBehaviour {
      */
     private void HandleDash ()
     {
-		if(disableMoveTimer <= 0f && !outOfWater && isDoubleClicking)
+        if(disableMoveTimer <= 0f && !outOfWater && isDoubleClicking)
         {
             // Create Dash Effect
             GameObject dashEffectInstance = EffectManager.Instance().SpawnDashEffect();
@@ -168,7 +168,7 @@ public class PlayerController : MonoBehaviour {
             currentMaxVelocity = maxDashVelocity + (maxVelocityRankMultiplier * GameManager.Instance().GetCurrentRank());
             
             velocity = direction.normalized * currentMaxVelocity;
-		}
+        }
     }
 
 
@@ -177,15 +177,15 @@ public class PlayerController : MonoBehaviour {
      */
     private void SetAcceleration ()
     {
-		acceleration = Vector3.zero;
+        acceleration = Vector3.zero;
 
-		if(disableMoveTimer <= 0f && !outOfWater && isClicking)
+        if(disableMoveTimer <= 0f && !outOfWater && isClicking)
         {
             // If the player is changing direction, increase acceleration by multiplier
             float accelerationBoost = 1f + oppsiteAccelerationBoost.Evaluate( Vector3.Dot(velocity.normalized, direction.normalized) );
             
             acceleration = direction * accelerationBoost * accelerationIntensity;
-		}
+        }
         else
         {
             velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * dragSpeed);
@@ -205,24 +205,24 @@ public class PlayerController : MonoBehaviour {
         // Handle moving in vs out of the water
         if (outOfWater)
         {
-			velocity += Time.deltaTime * outOfWaterGravity;
-			currentMaxVelocity = Mathf.Lerp(currentMaxVelocity, maxDashVelocity, Time.deltaTime * maxVelocityRestoreSpeed); // Set currentMaxVelocity back to default
-		}
+            velocity += Time.deltaTime * outOfWaterGravity;
+            currentMaxVelocity = Mathf.Lerp(currentMaxVelocity, maxDashVelocity, Time.deltaTime * maxVelocityRestoreSpeed); // Set currentMaxVelocity back to default
+        }
         else
         {
             // Velocity calculated from acceleration
-			velocity += acceleration * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime;
 
             // Rank Based velocity boost
-			velocity += acceleration.normalized * (accelerationRankMultiplier * GameManager.Instance().GetCurrentRank()) * Time.deltaTime;
-			
+            velocity += acceleration.normalized * (accelerationRankMultiplier * GameManager.Instance().GetCurrentRank()) * Time.deltaTime;
+            
             // Set currentMaxVelocity back to default
             currentMaxVelocity = Mathf.Lerp(
                 currentMaxVelocity,
                 maxVelocity + (maxVelocityRankMultiplier * GameManager.Instance().GetCurrentRank()),
                 Time.deltaTime * maxVelocityRestoreSpeed
             );
-		}
+        }
 
 
         if (velocity.magnitude > currentMaxVelocity)
@@ -230,11 +230,11 @@ public class PlayerController : MonoBehaviour {
             velocity = velocity.normalized * currentMaxVelocity;
         }
 
-		velocity.z = 0f;
+        velocity.z = 0f;
     }
 
 
-	/*
+    /*
      * Sets the animator state based on whether or not the player is sitting still, swimming, or dashing
      */
     private void SetAnimatorState ()
@@ -244,12 +244,12 @@ public class PlayerController : MonoBehaviour {
     }
 
 
-	/*
+    /*
      * Handles player rotation based on the direction the player is looking
      */
     private void HandleRotation ()
     {
-		transform.eulerAngles = new Vector3(
+        transform.eulerAngles = new Vector3(
             0f,
             0f,
             Mathf.LerpAngle(
@@ -261,27 +261,27 @@ public class PlayerController : MonoBehaviour {
     }
 
 
-	/*
+    /*
      * Handles player collisions with walls and reflects them in the opposite direction
      */
     private void HandleReflectonOnCollision ()
     {
-		RaycastHit2D hit = Physics2D.Raycast(this.transform.position, velocity, velocity.magnitude * Time.deltaTime, 1 << 8);
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, velocity, velocity.magnitude * Time.deltaTime, 1 << 8);
 
-		if(hit.collider != null) {
-			velocity = bounceSpeedDampen * (velocity - (2 * Vector3.Dot(velocity, (Vector3)hit.normal) * (Vector3)hit.normal));
-			disableMoveTimer = disableMoveMaxTime;
-		}
+        if(hit.collider != null) {
+            velocity = bounceSpeedDampen * (velocity - (2 * Vector3.Dot(velocity, (Vector3)hit.normal) * (Vector3)hit.normal));
+            disableMoveTimer = disableMoveMaxTime;
+        }
     }
 
 
-	/*
+    /*
      * Faces the player sprite in the correct direction by flipping localscale y
      */
     private void FaceSpriteInDirection ()
     {
         if ((0 < velocity.x && 0 > transform.localScale.y) || (0 > velocity.x && 0 < transform.localScale.y))
-			transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
     }
 
 }
